11aa7d5f2271ada6a788fb3f38c0ea1d08ffcf64
svchb <svchb@29e6be90-adf1-0310-8a7b-f9e772d029af>
2005-03-10 22:17:27 +0000

    Working copy...: changed intern behavior; cleaning...

--- trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
+++ trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
@@ -1,51 +1,87 @@
-ï»¿import javax.swing.JFrame;
+/*	
+#	Datei: Cgui_Window.java
+#     Entwickler: Michael Schmidt
+#     Status: Signing
+# 	Version: 0.0 22:52 10.03.05
+#	Beschreibung: Diese Klasse implementiert die grundlegenden Steuerungsfunktionen für ein Fenster
+#     		  Cgui-> Cgui_Windowmanager -> Cgui_Window
+#	ToDo: Pointobjekt(x,y); Methoden fertig machen; schließen; größe ändern; minimieren
+*/
+import javax.swing.JFrame;
 public class Cgui_Window extends JFrame
 {
-  //Erstellen eines globalen Objektes von Frame
+  /*
+  #	Globale Variablen-->
+  */
+  //Erstellen eines globalen Objektes von JFrame
   private JFrame fenster = new JFrame();
-  //Standardkonstruktor mit default Titel
+  /*
+  #	<--Globale Variablen
+  */
+  /*
+  #	Konstruktoren-->
+  */
+  //Standardkonstruktor
   public Cgui_Window()
   {
-    this.fenster.setTitle("");
-    this.fenster.show();
+    this.init_Fenster("");
   }
-  //Standardkonstruktor mit TitelÃƒÂ¼bergabe
+  //Standardkonstruktor mit Titelübergabe
   public Cgui_Window(final String Str_window_name)
   {
-    this.fenster.setTitle(Str_window_name);
-    this.fenster.show();
+    this.init_Fenster(FensterTitel);
   }
-  //Standardkonstrukor mit Titel und PixelgrÃƒÂ¶ÃƒÅ¸e
+  //Standardkonstrukor mit Titel und groeße in Pixeln
   public Cgui_Window (final String Str_window_name,final int ix,final int iy)
   {
     if(ix > iwidth() || iy > iheight())
     {
-      this.fenster.setSize(iwidth(),iheight());
+      this.fenster.setSize(i_get_Breite(), i_get_Höhe());
     }
     else
     {
       this.fenster.setSize(ix,iy);
     }
-    this.fenster.setTitle(Str_window_name);
-    this.fenster.show();
+    this.init_Fenster(FensterTitel);
   }
-  //Standardkonstruktor mit Titel und relativer GrÃƒÂ¶ÃƒÅ¸e
-  public Cgui_Window (final String Str_window_name,final double rx,final double ry)
+  //Standardkonstruktor mit Titel und relativer Groeße
+  public Cgui_Window (final String str_FensterTitel,final double rx,final double ry)
   {
-    this.fenster.setSize((int)(this.iwidth()*rx),(int)(this.iheight()*ry));
-    this.fenster.setTitle(Str_window_name);
-    this.fenster.show();
+    this.fenster.setSize((int)(this.i_get_Breite()*rx),(int)(this.i_get_Breite()*ry));
+    this.init_Fenster(FensterTitel);
   }
-  //Herausfinden der wirklichen BildschirmhÃƒÂ¶he
-  private int iheight()
+  /*
+  #	<--Konstruktoren
+  */
+  /*
+  #	Funktionen-->
+  */
+
+  //Herausfinden der wirklichen Bildschirmaufloesung(y)
+  private int i_get_Höhe()
   {
     return (getToolkit().getScreenSize().height);
   }
-  //Herausfinden der wirklichen Bildschirmbreite
-  private int iwidth()
+
+  //Herausfinden der wirklichen Bildschirmaufloesung(x)
+  private int i_get_Breite()
   {
     return (getToolkit().getScreenSize().width);
   }
 
+  //initialiesiert das Fenster
+  private void init_Fenster(String str_FensterTitel)
+  {
+    this.fenster.setTitle(str_FensterTitel);
+    this.fenster.show();
+  }
+
+  /*
+  #	<--Funktionen
+  */
+  /*
+  #	Methoden-->
+  */
+  public void setze_relSize
 }
 
