f37fe32e52a84b257856fd4efc69b2666fc21fcb
svchb <svchb@29e6be90-adf1-0310-8a7b-f9e772d029af>
2005-03-10 20:42:04 +0000

    Workbench upload (signing in progress)

---
+++ trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
@@ -0,0 +1,51 @@
+﻿import javax.swing.JFrame;
+public class Cgui_Window extends JFrame
+{
+  //Erstellen eines globalen Objektes von Frame
+  private JFrame fenster = new JFrame();
+  //Standardkonstruktor mit default Titel
+  public Cgui_Window()
+  {
+    this.fenster.setTitle("");
+    this.fenster.show();
+  }
+  //Standardkonstruktor mit TitelÃ¼bergabe
+  public Cgui_Window(final String Str_window_name)
+  {
+    this.fenster.setTitle(Str_window_name);
+    this.fenster.show();
+  }
+  //Standardkonstrukor mit Titel und PixelgrÃ¶ÃŸe
+  public Cgui_Window (final String Str_window_name,final int ix,final int iy)
+  {
+    if(ix > iwidth() || iy > iheight())
+    {
+      this.fenster.setSize(iwidth(),iheight());
+    }
+    else
+    {
+      this.fenster.setSize(ix,iy);
+    }
+    this.fenster.setTitle(Str_window_name);
+    this.fenster.show();
+  }
+  //Standardkonstruktor mit Titel und relativer GrÃ¶ÃŸe
+  public Cgui_Window (final String Str_window_name,final double rx,final double ry)
+  {
+    this.fenster.setSize((int)(this.iwidth()*rx),(int)(this.iheight()*ry));
+    this.fenster.setTitle(Str_window_name);
+    this.fenster.show();
+  }
+  //Herausfinden der wirklichen BildschirmhÃ¶he
+  private int iheight()
+  {
+    return (getToolkit().getScreenSize().height);
+  }
+  //Herausfinden der wirklichen Bildschirmbreite
+  private int iwidth()
+  {
+    return (getToolkit().getScreenSize().width);
+  }
+
+}
+
