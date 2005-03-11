a1c3c9811c9627e46cf4dd8dff7f003d67276bea
svchb <svchb@29e6be90-adf1-0310-8a7b-f9e772d029af>
2005-03-11 17:43:36 +0000

    -verion has been assigned Testing state!

--- trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
+++ trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
@@ -1,11 +1,11 @@
 /*	
 #	Datei: Cgui_Window.java
 #     Entwickler: Michael Schmidt
-#     Status: Signing
-# 	Version: 0.0 22:52 10.03.05
+#     Status: v.01->Testing
+# 	Version: 0.1 18:35 11.03.2005
 #	Beschreibung: Diese Klasse implementiert die grundlegenden Steuerungsfunktionen für ein Fenster
 #     		  Cgui-> Cgui_Windowmanager -> Cgui_Window
-#	ToDo: Pointobjekt(x,y); Methoden fertig machen; schließen; größe ändern; minimieren
+#	ToDo: Pointobjekt(x,y); schließen; minimieren; (alle) Methoden aus dieser Klasse nach (Cgui) Windowmanager; package
 */
 import javax.swing.JFrame;
 public class Cgui_Window extends JFrame
@@ -13,42 +13,49 @@ public class Cgui_Window extends JFrame
   /*
   #	Globale Variablen-->
   */
+
   //Erstellen eines globalen Objektes von JFrame
   private JFrame fenster = new JFrame();
+
   /*
   #	<--Globale Variablen
   */
   /*
   #	Konstruktoren-->
   */
+
+ 
   //Standardkonstruktor
-  public Cgui_Window()
+  private Cgui_Window()
   {
     this.init_Fenster("");
   }
+
   //Standardkonstruktor mit Titelübergabe
-  public Cgui_Window(final String Str_window_name)
+  public Cgui_Window(final String str_FensterTitel)
   {
-    this.init_Fenster(FensterTitel);
+    this.init_Fenster(str_FensterTitel);
   }
+
   //Standardkonstrukor mit Titel und groeße in Pixeln
-  public Cgui_Window (final String Str_window_name,final int ix,final int iy)
+  public Cgui_Window (final String Str_FensterTitel, final int i_SizeX, final int i_SizeY)
   {
-    if(ix > iwidth() || iy > iheight())
+    if(i_SizeX > i_getBreite() || i_SizeY > i_getHöhe())
     {
-      this.fenster.setSize(i_get_Breite(), i_get_Höhe());
+      this.set_relSize(i_getBreite(), i_getHöhe());
     }
     else
     {
-      this.fenster.setSize(ix,iy);
+      this.fenster.setSize(i_SizeX, i_SizeY);
     }
-    this.init_Fenster(FensterTitel);
+    this.init_Fenster(str_FensterTitel);
   }
+
   //Standardkonstruktor mit Titel und relativer Groeße
-  public Cgui_Window (final String str_FensterTitel,final double rx,final double ry)
+  public Cgui_Window (final String str_FensterTitel, final double drel_SizeX, final double drel_SizeY)
   {
-    this.fenster.setSize((int)(this.i_get_Breite()*rx),(int)(this.i_get_Breite()*ry));
-    this.init_Fenster(FensterTitel);
+    this.set_relSize(drel_SizeX, drel_SizeY);
+    this.init_Fenster(str_FensterTitel);
   }
   /*
   #	<--Konstruktoren
@@ -56,15 +63,20 @@ public class Cgui_Window extends JFrame
   /*
   #	Funktionen-->
   */
+  //ändert die Grösse
+  private void set_Size(final double drel_SizeX, final double drel_SizeY, final int i_SizeX, final int i_SizeY)
+  {
+	this.fenster.setSize((int)(i_SizeX * drel_SizeX),(int)(i_SizeY * drel_SizeY));
+  }
 
   //Herausfinden der wirklichen Bildschirmaufloesung(y)
-  private int i_get_Höhe()
+  private int i_getHöhe()
   {
     return (getToolkit().getScreenSize().height);
   }
 
   //Herausfinden der wirklichen Bildschirmaufloesung(x)
-  private int i_get_Breite()
+  private int i_getBreite()
   {
     return (getToolkit().getScreenSize().width);
   }
@@ -82,6 +94,19 @@ public class Cgui_Window extends JFrame
   /*
   #	Methoden-->
   */
-  public void setze_relSize
+  //ändert die relative Groeße des Fensters
+  public void setze_relSize(final double drel_SizeX, final double drel_SizeY)
+  {
+	this.set_Size( drel_SizeX, drel_SizeY, i_getBreite(), i_getHöhe());
+  }
+
+  //ändert die Groeße des Fensters
+  public void setze_Size(final int i_SizeX, final int i_SizeY)
+  {
+	this.set_Size(1.0d, 1.0d, i_SizeX, i_SizeY);
+  }
+  /*
+  #	<--Methoden
+  */
 }
 
