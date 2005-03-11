248053d0c1b117f427e98c0484f2b4ef1b16021b
svchb <svchb@29e6be90-adf1-0310-8a7b-f9e772d029af>
2005-03-11 17:58:10 +0000

    -fixed tippfehler

--- trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
+++ trunk/workbenchs/Sven Berger/Cgui/Cgui_Window.java
@@ -1,54 +1,77 @@
-/*	
-#	Datei: Cgui_Window.java
+/*  
+#   Datei: Cgui_Window.java
 #     Entwickler: Michael Schmidt
 #     Status: v.01->Testing
-# 	Version: 0.1 18:35 11.03.2005
-#	Beschreibung: Diese Klasse implementiert die grundlegenden Steuerungsfunktionen für ein Fenster
-#     		  Cgui-> Cgui_Windowmanager -> Cgui_Window
-#	ToDo: Pointobjekt(x,y); schließen; minimieren; (alle) Methoden aus dieser Klasse nach (Cgui) Windowmanager; package
+#   Version: 0.1 18:35 11.03.2005
+#   Beschreibung: Diese Klasse implementiert die grundlegenden Steuerungsfunktionen für ein Fenster
+#             Cgui-> Cgui_Windowmanager -> Cgui_Window
+#   ToDo: Pointobjekt(x,y); schließen; minimieren; (alle) Methoden aus dieser Klasse nach (Cgui) Windowmanager; package; move ifs to set_Size
 */
 import javax.swing.JFrame;
 public class Cgui_Window extends JFrame
 {
   /*
-  #	Globale Variablen-->
+  # Globale Variablen-->
   */
 
   //Erstellen eines globalen Objektes von JFrame
   private JFrame fenster = new JFrame();
 
   /*
-  #	<--Globale Variablen
+  # <--Globale Variablen
   */
   /*
-  #	Konstruktoren-->
+  # Konstruktoren-->
   */
 
  
   //Standardkonstruktor
   private Cgui_Window()
   {
+    this.set_relSize(0.7d, 0.7d);
     this.init_Fenster("");
   }
 
   //Standardkonstruktor mit Titelübergabe
   public Cgui_Window(final String str_FensterTitel)
   {
+    this.set_relSize(0.7d, 0.7d);
     this.init_Fenster(str_FensterTitel);
   }
 
-  //Standardkonstrukor mit Titel und groeße in Pixeln
-  public Cgui_Window (final String Str_FensterTitel, final int i_SizeX, final int i_SizeY)
+  //Standardkonstrukor mit groeße in Pixeln
+  public Cgui_Window (final int i_SizeX, final int i_SizeY)
   {
     if(i_SizeX > i_getBreite() || i_SizeY > i_getHöhe())
     {
-      this.set_relSize(i_getBreite(), i_getHöhe());
+      this.set_absSize(i_getBreite(), i_getHöhe());
     }
     else
     {
-      this.fenster.setSize(i_SizeX, i_SizeY);
+      this.set_absSize(i_SizeX, i_SizeY);
+    }
+    this.init_Fenster("");
+  }
+
+  //Standardkonstrukor mit Titel und groeße in Pixeln
+  public Cgui_Window (final String str_FensterTitel, final int i_SizeX, final int i_SizeY)
+  {
+    if(i_SizeX > i_getBreite() || i_SizeY > i_getHöhe())
+    {
+      this.set_absSize(i_getBreite(), i_getHöhe());
+    }
+    else
+    {
+      this.set_absSize(i_SizeX, i_SizeY);
     }
     this.init_Fenster(str_FensterTitel);
+  }
+
+  //Standardkonstruktor mit relativer Groeße
+  public Cgui_Window (final double drel_SizeX, final double drel_SizeY)
+  {
+    this.set_relSize(drel_SizeX, drel_SizeY);
+    this.init_Fenster("");
   }
 
   //Standardkonstruktor mit Titel und relativer Groeße
@@ -58,15 +81,15 @@ public class Cgui_Window extends JFrame
     this.init_Fenster(str_FensterTitel);
   }
   /*
-  #	<--Konstruktoren
+  # <--Konstruktoren
   */
   /*
-  #	Funktionen-->
+  # Funktionen-->
   */
   //ändert die Grösse
   private void set_Size(final double drel_SizeX, final double drel_SizeY, final int i_SizeX, final int i_SizeY)
   {
-	this.fenster.setSize((int)(i_SizeX * drel_SizeX),(int)(i_SizeY * drel_SizeY));
+    this.fenster.setSize((int)(i_SizeX * drel_SizeX),(int)(i_SizeY * drel_SizeY));
   }
 
   //Herausfinden der wirklichen Bildschirmaufloesung(y)
@@ -89,24 +112,23 @@ public class Cgui_Window extends JFrame
   }
 
   /*
-  #	<--Funktionen
+  # <--Funktionen
   */
   /*
-  #	Methoden-->
+  # Methoden-->
   */
   //ändert die relative Groeße des Fensters
-  public void setze_relSize(final double drel_SizeX, final double drel_SizeY)
+  public void set_relSize(final double drel_SizeX, final double drel_SizeY)
   {
-	this.set_Size( drel_SizeX, drel_SizeY, i_getBreite(), i_getHöhe());
+    this.set_Size( drel_SizeX, drel_SizeY, i_getBreite(), i_getHöhe());
   }
 
   //ändert die Groeße des Fensters
-  public void setze_Size(final int i_SizeX, final int i_SizeY)
+  public void set_absSize(final int i_SizeX, final int i_SizeY)
   {
-	this.set_Size(1.0d, 1.0d, i_SizeX, i_SizeY);
+    this.set_Size(1.0d, 1.0d, i_SizeX, i_SizeY);
   }
   /*
-  #	<--Methoden
+  # <--Methoden
   */
 }
-
